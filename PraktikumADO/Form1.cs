8a9786d187f7ce2bc88e7c7ea9a599ab58a2e5df
i-i-ijul <haqqikamvret@gmail.com>
2026-03-13 12:43:24 +0600

    menambahkan function btnUpdateMK_Click

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -120,7 +120,7 @@ namespace PraktikumADO
         private void Form1_Load(object sender, EventArgs e)
         {
 
-        }
+        } 
 
         private void btnUpdateMK_Click(object sender, EventArgs e)
         {
